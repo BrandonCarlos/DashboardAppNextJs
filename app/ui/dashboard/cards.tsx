dd006554faaf1989da4345fdc1ac0294daac65ad
Brandon Carlos <brandon_marzo@hotmail.com>
2024-07-30 20:39:47 -0300

    Streaming

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,7 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import {fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 
 
 const iconMap = {
@@ -16,16 +16,19 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+
+  const { numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
+
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      <Card title="Collected" value={(await fetchCardData()).totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={(await fetchCardData()).totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={(await fetchCardData()).numberOfInvoices} type="invoices" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
-        value={(await fetchCardData()).numberOfCustomers}
+        value={numberOfCustomers}
         type="customers"
       />
     </>
